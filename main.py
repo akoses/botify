bdd896fcc07783501ead9f60299e6f4a6486990b
Tosin Kuye <tkuye77@gmail.com>
2022-03-10 19:12:34 -0700

    Track events and redis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import datetime
 import os
+import time
 from dotenv import load_dotenv
 from discord.commands import Option, permissions
 from discord.ext import commands, tasks
@@ -18,17 +19,19 @@ from db import (
 	add_salaries, 
 	set_user_balance, 
 	get_entries, 
-	set_entries
+	set_entries, 
+	get_latest_events,
+	get_latest_jobs
 )
 
+from views import *
 from xp import assign_xp
 from utils import *
 from roles.roles import IGNORE_ROLES, ROLE_TO_SALARY
 from scheduler import *
 
-intents = discord.Intents().all()
 
-bot = commands.Bot(intents=intents)
+
 
 index_to_num = {
 	1: "one",
@@ -57,7 +60,7 @@ index_to_unicode = {
 	10:'🔟',
 }
 
-TRIVIA_PLAYERS = set()
+
 
 async def college_search(ctx:discord.AutocompleteContext):
 	college_roles = set(map(lambda x: x.name, ctx.interaction.guild.roles)) - IGNORE_ROLES
@@ -101,7 +104,58 @@ async def pay_salaries():
 		TRIVIA_PLAYERS.clear()
 
 
+@tasks.loop(time=[datetime.time(hour=8, minute=0, second=0)])
+async def get_trivia_question():
+	question, correct, answers = await get_trivia_question()
+	
+	if question and correct and answers:
+		trivia = {
+		"question": question,
+		"correct": correct,
+		"answers": answers
+		}
+		trivia_json = json.dumps(trivia)
+	redisClient.set("trivia", trivia_json)
+	redisClient.delete("trivia_players")
+	prize = determine_trivia_prize()
+	redisClient.set("trivia_prize", prize)
 
+@tasks.loop(secs=30)
+async def check_events():
+	events = await get_latest_events()
+	if events:
+		for event in events:
+			channel = bot.get_channel(event[5])
+			if channel:
+				embed = discord.Embed(title=event[2], description=event[3], color=0x00ff00)
+				embed.add_field(name="Hosted by", value=event[4])
+				embed.add_field(name="Date", value=event[6])
+				link =  event[7]
+				view = LinkView(link, "Event Link")
+				event_button = EventButton(str(event[0]), event[2])
+				view.add_item(event_button) 
+				await channel.send(embed=embed, view=view)
+				date = time.mktime(event[6].timetuple())
+				bot.loop.create_task(notify_event(event[0]))
+
+@tasks.loop(secs=30)
+async def check_jobs():
+	jobs = await get_latest_jobs()
+	if jobs:
+		for job in jobs:
+			channel = bot.get_channel(job[5])
+			if channel:
+				embed = discord.Embed(title=job[3], description=job[4], color=0x00ff00)
+				embed.add_field(name="Organization", value=job[5])
+				embed.add_field(name="Location", value=job[8])
+				embed.add_field(name="Disciplines", value=job[6])
+				link =  job[9]
+				view = LinkView(link, "Apply URL")
+				job_button = JobButton(str(job[0]), job[3])
+				view.add_item(job_button)
+				await channel.send(embed=embed, view=view)
+			
+						
 @bot.event
 async def on_ready():
 	"""
@@ -225,102 +279,15 @@ async def attendevent(ctx, event_id: Option(int, "Enter the event id")):
 
 
 
-class TriviaButton(discord.ui.Button):
-	def __init__(self, answer, correct, index):
-		super().__init__(
-            label=answer,
-            style=discord.enums.ButtonStyle.secondary,
-            custom_id=f'{index} {correct}',
-        )
-
-
-	async def callback(self, interaction):
-		correct = self.custom_id.split(" ")[1]
-		prize = determine_trivia_prize()
-		if correct == self.label:
-			await interaction.response.edit_message(content="That's correct! You've just won ${} coins! Come back tomorrow to play again.".format(prize), view=None)
-			set_user_balance(prize, interaction.user.id)
-		else:
-			await interaction.response.edit_message(content="That's incorrect! The correct answer is {}. You could have won ${} coins. Come back tomorrow to play again.".format(correct, prize), view=None)
-		await assign_xp(bot, "TRIVIA", interaction.user.id)
-		TRIVIA_PLAYERS.add(interaction.user.id)
-
-
-class AnswerView(discord.ui.View):
-	def __init__(self):
-		super().__init__(timeout=10)
-
-async def create_college_fn(guild, college):
-	"""
-	Create a college role for the server.
-	"""
-	if not guild:
-		guild = college['requested_by'].guild
-	
-	college_name = college['name'].rstrip().replace(' ', '-')
-	role = await guild.create_role(name=college_name, mentionable=True)
-	category = await guild.create_category(name=college_name, reason=college['reason'], position=6)
-	await category.set_permissions(role, read_messages=True, send_messages=True, connect=True, speak=True)
-	await category.set_permissions(guild.default_role, read_messages=False, connect=False)
-	await guild.create_text_channel(name=f"{college_name}-opportunities", category=category, reason=college['reason'])
-	await guild.create_text_channel(name=f"{college_name}-events", category=category, reason=college['reason'])
-	await guild.create_text_channel(name=f"{college_name}-general", category=category, reason=college['reason'])
-	
-	creator = college['requested_by']
-	len_roles = len(guild.roles)
-	await role.edit(position=(len_roles - 11))
-	await creator.add_roles(role)
-	await creator.send(f"Your new college {college_name} has been created!")
-	
-
-class CollegeRequestView(discord.ui.View):
-	def __init__(self, college_data):
-		super().__init__(timeout=None)
-		self.college = college_data
-	
-	@discord.ui.button(label="Approve", style=discord.enums.ButtonStyle.success)
-	async def approve(self, button, interaction):
-		await create_college_fn(interaction.guild, self.college)
-		await interaction.response.edit_message(content="College has been approved.", delete_after=10, view=None, embed=None)
-	@discord.ui.button(label="Deny", style=discord.enums.ButtonStyle.danger)
-	async def deny(self, button, interaction):
-		await interaction.response.edit_message(content="College denied.", delete_after=10, view=None, embed=None)
-		await interaction.user.send("Your college request has been denied.")
-
-
-class TriviaView(discord.ui.View):
-	def __init__(self):
-		super().__init__(timeout=30)
-
-	@discord.ui.button(label="Start",style=discord.ButtonStyle.primary)
-	async def callback(self, button, interaction):
-		question, correct, answers = await get_trivia_question()
-		if not question or not answers or not correct:
-			await interaction.response.edit_message(content="No more questions. Come back tomorrow to play again.", view=None)
-			TRIVIA_PLAYERS.clear()
-			return
-
-		answers.append(correct)
-		random.shuffle(answers)
-		
-		answerView = AnswerView()
-
-		for i, answer in enumerate(answers):
-			button = TriviaButton(answer, correct, i)
-			answerView.add_item(button)
-		
-		await interaction.response.edit_message(content=question, view=answerView)
-	
-
 @bot.slash_command(name="trivia", description="Play a trivia game to win coins! You can only play once per day.", guild_ids=[939394818428243999])
 async def trivia(ctx):
-	if ctx.interaction.user.id in TRIVIA_PLAYERS:
+	if redisClient.sismember('trivia-players', ctx.interaction.user.id):
 		await ctx.interaction.response.send_message("You've already played today! Come back tomorrow to play again.", ephemeral=True)
 		return
 	trivia_message = f"""
 	Hello **{ctx.interaction.user.name}**, welcome to the trivia game! \nYou will answer one question and if your answer is correct, you will be awarded between **1000** and **100,000** coins.\nYou will have **10** seconds to answer the question.\nPress the start button in order to begin!
 	"""
-	triviaView = TriviaView()
+	triviaView = TriviaView()  
 	triviaView.message = await ctx.interaction.response.send_message(trivia_message, ephemeral=True, view=triviaView)
 	triviaView.ctx = ctx
 	
---
+++ views.py
@@ -0,0 +1,104 @@
+import discord
+from utils import *
+from xp import assign_xp
+from db import *
+import json
+
+class LinkView(discord.ui.View):
+	def __init__(self, url, label):
+		super().__init__()
+		self.url = url
+		self.label = label
+		self.add_item(
+			discord.ui.Button(label=self.label, url=self.url)
+		)
+
+
+class EventButton(discord.ui.Button):
+	def __init__(self, event_id, event_name):
+		super().__init__(
+			label="Attend Event",
+			style=discord.enums.ButtonStyle.primary
+		)
+		self.id = event_id
+		self.name = event_name
+	async def callback(self, interaction):
+		redisClient.rpush(self.id, interaction.user.id)
+		await assign_xp(bot, "ATTEND_EVENT", interaction.user.id)
+		await interaction.user.send(content="You have successfully signed up to be notified of {}!".format(self.name))
+
+class JobButton(discord.ui.Button):
+	def __init__(self, job_id, job_name):
+		super().__init__(
+			label="Track Application",
+			style=discord.enums.ButtonStyle.primary, 
+		)
+		self.id = job_id
+		self.name = job_name
+	async def callback(self, interaction):
+		await assign_xp(bot, "APPLY", interaction.user.id)
+		await apply_to_job(interaction.user.id, self.id)
+		await interaction.user.send(content=f"You have successfully tracked the role {self.name}! You can check your other applications using the `/applications` command.")
+
+class CollegeRequestView(discord.ui.View):
+	def __init__(self, college_data):
+		super().__init__(timeout=None)
+		self.college = college_data
+	
+	@discord.ui.button(label="Approve", style=discord.enums.ButtonStyle.success)
+	async def approve(self, button, interaction):
+		await create_college_fn(interaction.guild, self.college)
+		await interaction.response.edit_message(content="College has been approved.", delete_after=10, view=None, embed=None)
+	@discord.ui.button(label="Deny", style=discord.enums.ButtonStyle.danger)
+	async def deny(self, button, interaction):
+		await interaction.response.edit_message(content="College denied.", delete_after=10, view=None, embed=None)
+		await interaction.user.send("Your college request has been denied.")
+
+
+class TriviaView(discord.ui.View):
+	def __init__(self):
+		super().__init__(timeout=30)
+
+	@discord.ui.button(label="Start",style=discord.ButtonStyle.primary)
+	async def callback(self, button, interaction):
+		trivia = json.loads(redisClient.get("trivia"))
+		question, correct, answers = trivia['question'], trivia['correct'], trivia['answers']
+		
+		answers.append(correct)
+		random.shuffle(answers)
+		
+		answerView = AnswerView()
+
+		for i, answer in enumerate(answers):
+			button = TriviaButton(answer, correct, i)
+			answerView.add_item(button)
+		
+		await interaction.response.edit_message(content=question, view=answerView)
+		redisClient.sadd("trivia-players", str(interaction.user.id))
+		
+
+class AnswerView(discord.ui.View):
+	def __init__(self):
+		super().__init__(timeout=10)
+
+
+class TriviaButton(discord.ui.Button):
+	def __init__(self, answer, correct, index):
+		super().__init__(
+            label=answer,
+            style=discord.enums.ButtonStyle.secondary,
+            custom_id=f'{index} {correct}',
+        )
+
+
+	async def callback(self, interaction):
+		correct = self.custom_id.split(" ")[1]
+		prize = redisClient.get('trivia-prize')
+		if correct == self.label:
+			await interaction.response.edit_message(content="That's correct! You've just won ${} coins! Come back tomorrow to play again.".format(prize), view=None)
+			set_user_balance(prize, interaction.user.id)
+		else:
+			await interaction.response.edit_message(content="That's incorrect! The correct answer is {}. You could have won ${} coins. Come back tomorrow to play again.".format(correct, prize), view=None)
+		await assign_xp(bot, "TRIVIA", interaction.user.id)
+		
+
