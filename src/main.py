9a8362fd9513ce3811f0a7e74f467076cccd9e07
Tosin Kuye <tkuye77@gmail.com>
2022-03-15 13:23:16 -0600

    chegg

--- src/main.py
+++ src/main.py
@@ -423,7 +423,7 @@ async def assign_xp_to_user(ctx,
 
 
 @bot.slash_command(name="chegg", description="Convert's chegg link to unblocked content.", guild_ids=guild_ids)
-@permissions.has_any_role(*role_or_higher("Graduate"))
+@permissions.has_any_role(*role_or_higher("Teaching Assistant"))
 async def chegg(ctx,
 	link: Option(str, "Enter the chegg link")):
 	await ctx.defer()
